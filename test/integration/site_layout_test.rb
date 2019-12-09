936c19864593564982898c9fd6fa4040a4520f83
mike <michgoldennathan17@gmail.com>
2019-12-09 09:12:42 +0100

    Test
    Add integration tests for signup page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,7 +8,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
    end
 end
