c1c4c3ef10927660084baca4b4e469a44c2081a8
mike <michgoldennathan17@gmail.com>
2019-12-13 21:34:45 +0100

    [Test] Add tests for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_153217) do
+ActiveRecord::Schema.define(version: 2019_12_13_181514) do
 
   create_table "static_pages", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_153217) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
