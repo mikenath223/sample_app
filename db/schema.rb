f7344b1e85dc80a1493235dbf277e0d13ea8f52a
mike <michgoldennathan17@gmail.com>
2019-12-13 17:06:48 +0100

    [Feature] Add users model and migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_223205) do
+ActiveRecord::Schema.define(version: 2019_12_13_153217) do
 
   create_table "static_pages", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
