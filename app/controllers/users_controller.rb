4ee66891d776f2f1852d7ffdcdd746460ffb32fb
mike <michgoldennathan17@gmail.com>
2019-12-19 13:33:11 +0100

    [Feature] Add new action, Update routes, Add scss to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
 end
