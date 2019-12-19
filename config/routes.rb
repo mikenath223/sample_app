29641abfa60dbb5e645268670db58d14be101642
mike <michgoldennathan17@gmail.com>
2019-12-19 22:34:49 +0100

    Finnish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  get '/signup', to: 'users#create'
   resources :users
 end
