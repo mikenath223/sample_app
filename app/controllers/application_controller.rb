227d6e849af180752c33baba89d5cb20d129729b
mike <michgoldennathan17@gmail.com>
2019-12-20 22:07:10 +0100

    [Feature] Add login functionality and corresponding integration tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,5 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: 'hello, world!'
-  end
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
