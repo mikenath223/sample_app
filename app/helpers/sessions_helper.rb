96a336fa8901de717b3cd20f066bb4529f7b8b3c
mike <michgoldennathan17@gmail.com>
2019-12-22 08:22:30 +0100

    [Feature] Create forgetting users feature

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,7 +28,14 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
