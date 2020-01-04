8030d33a2a8c67241aa58bad0bed7b3fe5f0abd6
mike <michgoldennathan17@gmail.com>
2020-01-04 19:26:34 +0100

    Revert to previous commit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,48 +1,53 @@
 module SessionsHelper
-  def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.digest(remember_token))
-    self.current_user = user
+  def log_in(user)
+    session[:user_id] = user.id
   end
 
-  def signed_in?
-    !current_user.nil?
+  #Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
-  def current_user=(user)
-    @current_user = user
-  end
-
+  # Returns the current logged-in user (if any).
   def current_user
-    remember_token = User.digest(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
-  end
-
-  def current_user?(user)
-    user == current_user
-  end
-
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
-  def sign_out
-    current_user.update_attribute(:remember_token,
-                                  User.digest(User.new_remember_token))
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
     cookies.delete(:remember_token)
-    self.current_user = nil
   end
 
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
   def redirect_back_or(default)
-    redirect_to(session[:return_to] || default)
-    session.delete(:return_to)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
   end
 
+  # Stores the URL trying to be accessed.
   def store_location
-    session[:return_to] = request.url if request.get?
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,7 @@
 User.create!(name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
-  password_confirmation: "foobar",
-  admin: true)
+  password_confirmation: "foobar")
 
 99.times do |n|
 name  = Faker::Name.name
