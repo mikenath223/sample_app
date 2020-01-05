dfa6d41c2e7e8d8280e6d19d9236dc5184b1780c
mike <michgoldennathan17@gmail.com>
2020-01-05 21:18:45 +0100

    [Test] Create test for default_scope method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
 
+  has_many :microposts
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
