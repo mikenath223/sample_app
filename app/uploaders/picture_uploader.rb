94c2d2227c7c7ad48b83ddf5d9d919371e4de84e
mike <michgoldennathan17@gmail.com>
2020-01-07 01:52:23 +0100

    [Fix] Remove storage:fog on prodcution env

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,7 +7,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   if Rails.env.production?
-    storage :fog
+    #do nothing
   else
     storage :file
   end
